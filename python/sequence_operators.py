43c889ead3f58cffa1ab73903641c625c4ec6b26
Sergio Gallegos <gallegoshinojosa@gmail.com>
2022-03-22 21:33:53 -0600

    lameravena

---
+++ python/sequence_operators.py
@@ -0,0 +1,9 @@
+string1 = "he's "
+string2 = "probably "
+string3 = "pinging "
+string4 = "for the "
+string5 = "fjords"
+
+print(string1 + string2 + string3 + string4 + string5)
+
+print("he's " "probably " "pinging " "for the " "fjords")

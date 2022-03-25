98a0afc6ab613575f0bdbeb07313231ba4ebb977
Sergio Gallegos <gallegoshinojosa@gmail.com>
2022-03-25 12:14:01 -0600

    sergiogallegos

---
+++ python/repfields.py
@@ -0,0 +1,4 @@
+age = 24
+print("My age is {0} years".format(age))
+
+print()
--- python/sequence_operators.py
+++ python/sequence_operators.py
@@ -7,3 +7,20 @@ string5 = "fjords"
 print(string1 + string2 + string3 + string4 + string5)
 
 print("he's " "probably " "pinging " "for the " "fjords")
+
+print("Hello " * 5)
+print("Hello " * (5+4))
+print("Hello " * 5 + "4")
+
+today = "friday"
+print("day" in today)        # True
+print("fri" in today)        # True
+print("thur" in today)       # False
+print("parrot" in "fjord")   # False
+
+
+print("put your name: ")
+a = input() 
+print("put yor age :")
+b = input()
+print(a + b)

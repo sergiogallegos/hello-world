67e9e20f79123b595884e82ecb2e53136b9c1344
Sergio Gallegos <gallegoshinojosa@gmail.com>
2022-03-27 19:57:44 -0600

    sergiogallegos

--- python/repfields.py
+++ python/repfields.py
@@ -1,4 +1,5 @@
 age = 24
 print("My age is {0} years".format(age))
 
-print()
+print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6} and {7}"
+			.format(31, "Jan", "Dec", "Mar", "May", "Jul", "Aug", "Oct", "Dec"))

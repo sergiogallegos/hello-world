3166f54447558d7150e7c45c670237a3faec7d82
Sergio <gallegoshinojosa@gmail.com>
2022-03-31 10:56:16 -0600

    sg

--- python/formatting.py
+++ python/formatting.py
@@ -2,3 +2,16 @@ for i in range(1, 13):
 	print("No. {0:2} squared is {1:3} and cubed is {2:4}".format(i, i ** 2, i ** 3))
 
 print()
+
+for i in range(1, 13):
+	print("No. {0:2} squared is {1:<3} and cubed is {2:^4}".format(i, i ** 2, i ** 3))
+
+print()
+
+print("Pi is approximately {0:12}".format(22/7))
+print("Pi is approximately {0:12f}".format(22/7))
+print("Pi is approximately {0:12.50f}".format(22/7))
+print("Pi is approximately {0:52.5}".format(22/7))
+print("Pi is approximately {0:62.50f}".format(22/7))
+print("Pi is approximately {0:<72.50f}".format(22/7))
+print("Pi is approximately {0:<72.54f}".format(22/7))

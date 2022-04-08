bd15936db46ccfaef157dfe95284e26a372e9464
sergio <sergio@pop-os.localdomain>
2022-04-08 10:50:52 -0600

    sg

--- python/prgflow/guessinggame.py
+++ python/prgflow/guessinggame.py
@@ -5,16 +5,26 @@ guess = int(input())
 
 if guess < answer:
     print("Please guess higher")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it")
+    else:
+        print("sorry, you have not guessed correectly")
 elif guess > answer:
     print("Please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it")
+    else:
+        print("sorry, you have not guessed correectly")
 else:
     print("You got it first time")
 
 
 # Variables Excersice
 
-x = input("Put X value")
-y = input("Put Y value")
+x = input("Put X value: ")
+y = input("Put Y value: ")
 
 if x > y:
     print("x is greater than y")

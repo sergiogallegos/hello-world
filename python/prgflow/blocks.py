e3b9bc1d3917237d76a604b6fdaf4001b193b0a2
Sergio Gallegos <gallegoshinojosa@gmail.com>
2022-04-07 21:40:40 -0600

    sg

--- python/prgflow/blocks.py
+++ python/prgflow/blocks.py
@@ -1,3 +1,10 @@
-for i in range(1, 13): 
-    print("No. {} squred is {} and cubed {:4}".format(i, i ** 2,i **3))
-    print("*" * 80)
+#for i in range(1, 13): 
+#    print("No. {} squred is {} and cubed {:4}".format(i, i ** 2,i **3))
+#    print("*" * 80)
+
+name = input("Please enter your name: ")
+age = int(input("How old are you, {}? ".format(name)))
+print(age)
+
+if age>= 18:
+    print("You are old enough to vote")

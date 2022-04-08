8f7ce6d82c3d8210ef9093b6039f5a1ab26087fc
Sergio Gallegos <gallegoshinojosa@gmail.com>
2022-04-07 22:21:45 -0600

    sg

--- python/prgflow/guessinggame.py
+++ python/prgflow/guessinggame.py
@@ -9,3 +9,16 @@ elif guess > answer:
     print("Please guess lower")
 else:
     print("You got it first time")
+
+
+# Variables Excersice
+
+x = input("Put X value")
+y = input("Put Y value")
+
+if x > y:
+    print("x is greater than y")
+elif x == y:
+    print("x equals y")
+else:
+    print("x is smaller than y")

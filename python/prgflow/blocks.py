02627f0e18c75dd96073c8ccff239d3eb8957dd6
Sergio Gallegos <gallegoshinojosa@gmail.com>
2022-04-07 21:46:35 -0600

    sg

--- python/prgflow/blocks.py
+++ python/prgflow/blocks.py
@@ -6,5 +6,15 @@ name = input("Please enter your name: ")
 age = int(input("How old are you, {}? ".format(name)))
 print(age)
 
-if age>= 18:
+if age >= 18:
     print("You are old enough to vote")
+    print("Please put an X in the box")
+else:
+    print("Please come back in {0} years".format(18 - age))
+
+
+if age < 18:
+    print("Please come back in {0} years".format(18 - age))
+else:
+    print("You are old enough to vote")
+    print("Please put an X in the box")
